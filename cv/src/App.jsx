21887b7a6e9df933a2e025b10920878818a5c960
fmps91 <fmps92@gmail.com>
2026-07-08 10:23:16 -0500

    2.0.0.0.11

--- cv/src/App.jsx
+++ cv/src/App.jsx
@@ -15,10 +15,10 @@ import MultiSelect from './components/selectLanguage/MultiSelect';
 function App() {
 
   const [showConfig, setShowConfig] = useState(false);
-  const [showATS, setShowATS] = useState(true);
-  const [showCV, setShowCV] = useState(false);
+  const [showATS, setShowATS] = useState(false);
+  const [showCV, setShowCV] = useState(true);
   const [valueCheck, setValueCheck] = useState(false);
-  const [leng, setLeng] = useState('en');
+  const [leng, setLeng] = useState('es');
   const [selectedLanguage, setSelectedLanguage] = useState('Inglés');
   const [label, setLabel] = useState('CV')
 
